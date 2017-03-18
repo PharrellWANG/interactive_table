c00c0ebd7955ec804604cbba269cf91854ba7ac3
PharrellWANG <15113029g@connect.polyu.hk>
2017-03-18 13:06:28 +0800

    frame built

--- cost_of_living/views.py
+++ cost_of_living/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def cost_of_living(request):
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    content = {
+
+    }
+    return render(request, 'main.html', content)
