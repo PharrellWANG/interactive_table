7d61ae15a5777a61ae8790e2ad912185ce1b43c6
PharrellWANG <15113029g@connect.polyu.hk>
2017-03-22 23:14:19 +0800

    new map added

--- cost_of_living/views.py
+++ cost_of_living/views.py
@@ -4,9 +4,16 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def cost_of_living(request):
+def predictor(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
     content = {
 
     }
-    return render(request, 'main.html', content)
+    return render(request, 'predictor.html', content)
+
+def good_map(request):
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    content = {
+
+    }
+    return render(request, 'map.html', content)
